f3e30a85716ad95573821a08e7bab69a1533103e
BetoSalas2015 <roberto.salazar@upaep.mx>
2020-04-21 13:33:11 -0500

    Avance: Applets

--- Calculadora3.java
+++ Calculadora3.java
@@ -3,19 +3,20 @@
  * Write a description of class Calculadora2 here.
  * 
  * @author Roberto Salazar Márquez
- * @version 2.0
+ * @version 3.0
  */
 
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
-public class Calculadora2 extends Frame
+public class Calculadora3 extends JFrame
 {
     // instance variables - replace the example below with your own
-    private Button b1, b2, b3, b4, b5, b6,b7, b8, b9, b10;
-    private Button b11, b12, b13, b14, b15, b16, b17;
-    private TextField display;
-    private Panel teclado, displ;
+    private JButton b1, b2, b3, b4, b5, b6,b7, b8, b9, b10;
+    private JButton b11, b12, b13, b14, b15, b16, b17;
+    private JTextField display;
+    private JPanel teclado, displ;
     private boolean punto = true, op = true;
     private char c = ' ';
     private double num1 = 0.0, num2 = 0.0, res = 0.0;
@@ -24,18 +25,18 @@ public class Calculadora2 extends Frame
     /**
      * Constructor for objects of class Calculadora
      */
-    public Calculadora2()
+    public Calculadora3()
     {
         // initialise instance variables
-        b1 = new Button("1"); b2 = new Button("2");
-        b3 = new Button("3"); b4 = new Button("4");
-        b5 = new Button("5"); b6 = new Button("6");
-        b7 = new Button("7"); b8 = new Button("8");
-        b9 = new Button("9"); b10 = new Button("0");
-        b11 = new Button("+"); b12 = new Button("-");
-        b13 = new Button("*"); b14 = new Button("/");
-        b15 = new Button("="); b16 = new Button(".");
-        b17 = new Button("C");
+        b1 = new JButton("1"); b2 = new JButton("2");
+        b3 = new JButton("3"); b4 = new JButton("4");
+        b5 = new JButton("5"); b6 = new JButton("6");
+        b7 = new JButton("7"); b8 = new JButton("8");
+        b9 = new JButton("9"); b10 = new JButton("0");
+        b11 = new JButton("+"); b12 = new JButton("-");
+        b13 = new JButton("*"); b14 = new JButton("/");
+        b15 = new JButton("="); b16 = new JButton(".");
+        b17 = new JButton("C");
         
         b1.addActionListener(new BotonNumerico());
         b2.addActionListener(new BotonNumerico());
@@ -56,9 +57,9 @@ public class Calculadora2 extends Frame
         b17.addActionListener(new BotonC());        
         
         
-        display = new TextField("0");
-        teclado = new Panel();
-        displ = new Panel();
+        display = new JTextField("0");
+        teclado = new JPanel();
+        displ = new JPanel();
         
         teclado.setLayout(new GridLayout(4, 4, 2, 2));
         teclado.add(b7);
